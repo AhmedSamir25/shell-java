9bccdafb75c8eb8c2657dd0dc219b22e913443b4
Ahmed Samir <ahmed_samir3@outlook.com>
2025-05-08 20:49:45 +0300

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,7 @@ public class Main {
             }
             String input = scanner.nextLine();
             System.out.println(input + ": command not found");
-            if(input.equals("0")){
+            if(input.equals("exit 0")){
                 break;
             }
         }
