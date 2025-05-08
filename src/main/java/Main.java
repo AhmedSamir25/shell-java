0fa1355fa6fa29d2f04ccf71869e159dc69d7e98
Ahmed Samir <ahmed_samir3@outlook.com>
2025-05-08 20:35:55 +0300

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,14 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
-        System.out.print("$ ");
-
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(input + ": command not found");
+        while (true) {
+            System.out.print("$ ");
+            if (!scanner.hasNextLine()) {
+                break;
+            }
+            String input = scanner.nextLine();
+            System.out.println(input + ": command not found");
+        }
     }
 }
