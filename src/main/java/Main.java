24b395d7fac1443bc95b7f7defe6fd519cfeb71e
Ahmed Samir <ahmed_samir3@outlook.com>
2025-05-08 20:58:52 +0300

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        // Uncomment this block to pass the first stage
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.print("$ ");
@@ -14,7 +13,11 @@ public class Main {
             if(input.equals("exit 0")){
                 break;
             }
-            System.out.println(input + ": command not found");
+            if(input.contains("echo")){
+                System.out.println(input);
+            }else{
+                System.out.println(input + ": command not found");
+            }
         }
     }
 }
