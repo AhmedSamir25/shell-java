a1da6e59bc204d0da610b3c4a07f23d3fd22c65c
Ahmed Samir <ahmed_samir3@outlook.com>
2025-05-26 17:44:50 +0300

    fix syntax support backslashes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -179,6 +179,7 @@ public class Main {
                         }
                         continue;
                     }
+                }
                 current.append('\\');
                 continue;
             }
